e6f033c720c717ccb0838416c1a03198f1dc4e20
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-01 21:22:08 +0530

    testcases for second and third position done. TC1.2, TC1.3 successfull

--- MaximumProblem/src/com/MainTest.java
+++ MaximumProblem/src/com/MainTest.java
@@ -6,10 +6,10 @@ class MainTest {
 
     @org.junit.jupiter.api.Test
     public void testFindMax(){
-        Integer x = 8;
-        Integer y = 5;
-        Integer z = 2;
-        Assertions.assertEquals(8, FindMaximum.findMax(x,y,z));
+        Assertions.assertEquals(8, FindMaximum.findMax(8,5,2));// returns first position TC1.1
+        Assertions.assertEquals(34,FindMaximum.findMax(5, 34, 25));// returns second position TC1.2
+        Assertions.assertEquals(90,FindMaximum.findMax(54,18,90));// returns third position TC1.3
     }
+
 
 }
